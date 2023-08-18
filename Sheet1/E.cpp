d39c26985f1204e12065b36b9947aa1fbb58e54f
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-18 15:40:32 +0300

    E done

---
+++ Sheet1/E.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+#include <cmath>
+using namespace std;
+int main(){
+    float r;
+    cin>>r;
+    double area = 3.141592653 * pow(r, 2);
+    cout<<fixed << setprecision(9)<<area;
+    return 0;
+}
