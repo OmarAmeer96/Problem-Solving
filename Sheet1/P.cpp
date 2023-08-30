fc49e0ded189c075fbc3c330e2b6f9665c0c28d6
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-30 14:48:34 +0300

    P done

--- Sheet1/P.cpp
+++ Sheet1/P.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
+    int num;
+    cin >> num;
+    string numberStr = to_string(num);
+    int firstDigit = (int)numberStr[0];
+    if (firstDigit % 2 == 0)
+    {
+        cout << "EVEN";
+    }
+    else
+    {
+        cout << "ODD";
+    }
+    return 0;
 }
