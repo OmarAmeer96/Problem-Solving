85cd011cb11e8d1c4da4bfa69e1952ec9281e749
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-30 14:31:18 +0300

    N Added

--- Sheet1/N.cpp
+++ Sheet1/N.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 int main()
 {
-    cout << "Jello, World!";
+    char in;
+    cin>>in;
+    int asciiBefore = (int)in;
+    int asciiAfter = 0;
+    if (asciiBefore < 97)
+    {
+        asciiAfter = asciiBefore + 32;
+    }else{
+        asciiAfter = asciiBefore - 32;
+    }
+    cout<<(char)asciiAfter;
+    return 0;
 }
